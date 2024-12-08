1db967bcae54b7f3da52eac90e0c31960fec8b45
CqurFar <ewfr454@mail.ru>
2024-12-08 12:58:38 +0300

    Обновление ветки

--- modeling/__init__.py
+++
@@ -1,6 +0,0 @@
-from .formation import TrainFormation
-from .augmentation import TrafficAugmentor
-from .time_series import TrafficJamsPredictor
-
-
-__all__ = ["TrainFormation", "TrafficAugmentor", "TrafficJamsPredictor"]
--- parsing/__init__.py
+++
@@ -1,6 +0,0 @@
-from .weather import WeatherParser
-from .score import ScoreParser
-from .crash import CrashRoads
-
-
-__all__ = ["WeatherParser", "ScoreParser", "CrashRoads"]
